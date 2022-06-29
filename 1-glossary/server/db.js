0e7aa0dd5ab65069361dd0a09e1422c24264a754
Nicholas Kempkes <nicholas.kempkes@gmail.com>
2022-06-29 12:13:13 -0500

    DB should be working.

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,4 +1,48 @@
 const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/glossary')
+  .then(() => {console.log('\x1b[33m%s\x1b[0m', 'MONGO CONNECTED')})
+  .catch(err => console.log(err));
+
+
+// Schema
+const glossarySchema = mongoose.Schema({
+  word: String,
+  definition: String
+});
+
+// Models
+const Glossary = mongoose.model('Glossary', glossarySchema);
+
+// Functions
+
+let addOrUpdate = (data) => {
+    return Glossary.findOneAndUpdate(
+      {word: data.word},
+      {...data},
+      {upsert: true}
+      )
+    .then(() => {console.log('\x1b[33m%s\x1b[0m', 'ENTRY CREATED')});
+}
+
+let remove = (query) => {
+  return Glossary.deleteOne({word: query.word})
+    .then(() => {console.log('\x1b[33m%s\x1b[0m', 'DELETED')});
+}
+let getOne = (word) => {
+  return Glossary.find({word: word})
+    .then(() => {console.log('\x1b[33m%s\x1b[0m', 'ONE FOUND')});
+}
+let getAll = () => {
+  return Glossary.find({})
+    .then(() => {console.log('\x1b[33m%s\x1b[0m', 'QUERY ALL')});
+}
+
+// console.log(addOrUpdate({word: "potato", definition: "Edible root thing that Hobbits like"}));
+// console.log(getAll({word: "potato", definition: "Edible root thing that Hobbits like"}));
+//console.log(remove({word: "potato", definition: "Edible root thing that Hobbits like"}));
+
+module.exports = {addOrUpdate, remove, getAll};
+
 
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,18 +1,52 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-
 const app = express();
+const { addOrUpdate, remove, getAll } = require("./db.js");
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/**** 
- * 
- * 
+app.use(express.json());
+
+app.get("/glossary", (req, res) => {
+  console.log(req.body.word);
+  if (req.body.word === undefined) {
+    getAll()
+      .then(function(response) {
+        return res.json(response);
+      })
+      .catch(function(err) {
+        return (res.status(500).json({message: "SERVER BROKE @ GET ALL"}))
+      });
+  } else {
+    getOne(req.body.word)
+      .then(function (response) {
+        return res.json(response);
+      })
+      .catch(function (err) {
+        return (res.status(500).json({ message: "SERVER BROKE @ GET ONE" }));
+      });
+  }
+});
+
+app.post("/glossary", (req, res) => {
+  addOrUpdate(req.body)
+    .then(function() {
+      res.sendStatus(201);
+    })
+    .catch(function(err) {
+      return (res.status(500).json({message: "SERVER BROKE @ POST"}));
+    })
+})
+
+
+/****
+ *
+ *
  * Other routes here....
  *
- * 
+ *
  */
 
 app.listen(process.env.PORT);
